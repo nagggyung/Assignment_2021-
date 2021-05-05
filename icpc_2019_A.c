09994bcc26383393a72cd73404f32d0c29f19402
nagggyung <74478432+nagggyung@users.noreply.github.com>
2021-05-05 13:34:53 +0900

    Add files via upload

---
+++ icpc_2019_A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int A[1001] = { 1,1 };
+    int n;
+    for (int i = 2; i < 1001; i++) {
+        int value = 1;
+        while (1) {
+            A[i] = value;
+            int k = 1;
+            for (k = 1; 2 * k <= i; k++) {
+                if (A[i] - A[i - k] == A[i - k] - A[i - 2 * k]) break;
+            }
+            if (i - 2 * k < 0)
+                break; 
+            else
+              value++;
+        }
+    }
+    printf("n의 값을 입력하세요: ");
+    scanf_s("%d", &n);
+    printf("%d\n", A[n]);
+    return 0;
+}
